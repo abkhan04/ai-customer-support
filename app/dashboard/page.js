735a16ffc9793a39cdbff45874ce06b1379a1bf4
Sebastian Bury <sebastianbury2004@gmail.com>
2024-08-09 19:16:50 +0100

    Updated dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,55 +1,4 @@
-// 'use client';
-// ---- BILL'S CODE FROM TUTORIAL ----
-// import { useState, useEffect } from 'react';
-// import { Box, Stack, TextField, Button } from '@mui/material';
 
-// export default function Home() {
-//     const [messages, setMessages] = useState([
-//         { role: 'assistant', content: 'Welcome to Chat Support! How can I help you today?' },
-//     ]);
-
-//     const sendMessage = async (message) => {
-//         setMessage('');
-//         setMessages((messages) => [...messages, { role: 'user', content: message }]);
-//         const response = fetch('/api/chat', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify([...messages, { role: "user", content: message }]),
-//         }).then(async (res) => {
-//             const reader = res.body.getReader();
-//             const decoder = new TextDecoder();
-//             let result = '';
-//             return reader.read().then(function processText({ done, value }) {
-//                 if (done) {
-//                     return result;
-//                 }
-//                 const text = decoder.decode(value || new Uint8Array(), { stream: true });
-//                 setMessages((messages) => {
-//                     let lastMessage = messages[messages.length - 1];
-//                     let otherMessages = messages.slice(0, messages.length - 1);
-
-//                     return [...otherMessages,
-//                     { ...lastMessage, content: lastMessage.content + text }];
-//                 })
-
-//                 return reader.read().then(processText);
-//             });
-//         });
-//     };
-
-//     const [message, setMessage] = useState('');
-//     return (
-//         <Box
-//             height="100vh"
-//             width="100vw"
-//             display="flex"
-//             flexDirection="column"
-//             justifyContent="center"
-//             alignItems="center"
-//             bgcolor="background.default"
-//         >
 //             <Stack direction="column" width="500px" height="700px" border="1px solid black" padding={2} spacing={3}>
 //                 <Stack spacing={2} direction="column" flexGrow={1} overflow="auto">
 //                     {
@@ -71,23 +20,10 @@
 //                         ))
 //                     }
 //                 </Stack>
-//                 <Stack direction="row" spacing={2}>
-//                     <TextField
-//                         label="Type a message"
-//                         fullWidth
-//                         value={message}
-//                         onChange={(e) => setMessage(e.target.value)}
-//                     />
-//                     <Button variant="contained" color="primary" onClick={sendMessage}>
-//                         Send
-//                     </Button>
-//                 </Stack>
-//             </Stack>
-//         </Box>
-//     )
-// }
-// ---- END OF BILL'S CODE ----
 
+'use client';
+
+import { useState, useEffect } from 'react';
 import {
     Bird,
     Book,
@@ -134,47 +70,58 @@ import {
     TooltipProvider,
 } from "@/components/ui/tooltip"
 
+import { RiRobot2Line } from "react-icons/ri";
+import Link from "next/link";
+
 export default function Dashboard() {
+    const [messages, setMessages] = useState([
+        { role: 'assistant', content: 'Welcome to Chat Support! How can I help you today?' },
+    ]);
+
+    const sendMessage = async (message) => {
+        setMessage('');
+        setMessages((messages) => [...messages, { role: 'user', content: message }]);
+        const response = fetch('/api/chat', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify([...messages, { role: "user", content: message }]),
+        }).then(async (res) => {
+            const reader = res.body.getReader();
+            const decoder = new TextDecoder();
+            let result = '';
+            return reader.read().then(function processText({ done, value }) {
+                if (done) {
+                    return result;
+                }
+                const text = decoder.decode(value || new Uint8Array(), { stream: true });
+                setMessages((messages) => {
+                    let lastMessage = messages[messages.length - 1];
+                    let otherMessages = messages.slice(0, messages.length - 1);
+
+                    return [...otherMessages,
+                    { ...lastMessage, content: lastMessage.content + text }];
+                })
+
+                return reader.read().then(processText);
+            });
+        });
+    };
+
+
+    const [message, setMessage] = useState('');
+
     return (
         <TooltipProvider>
             <div className="grid h-screen w-full pl-[56px]">
-                <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
-                    <div className="border-b p-2">
-                        <Button variant="outline" size="icon" aria-label="Home">
-                            <Triangle className="size-5 fill-foreground" />
+                <aside className="inset-y fixed left-0 z-20 flex h-full flex-col border-r-2">
+                    <div className="border-b-2 p-2">
+                        <Button size="icon" aria-label="Home" className="border-0 shadow-none hover:shadow-none">
+                            <Link href="/"><RiRobot2Line className="size-7 text-sky-500" /></Link>
                         </Button>
                     </div>
                     <nav className="grid gap-1 p-2">
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <Button
-                                    variant="ghost"
-                                    size="icon"
-                                    className="rounded-lg bg-muted"
-                                    aria-label="Playground"
-                                >
-                                    <SquareTerminal className="size-5" />
-                                </Button>
-                            </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
-                                Playground
-                            </TooltipContent>
-                        </Tooltip>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <Button
-                                    variant="ghost"
-                                    size="icon"
-                                    className="rounded-lg"
-                                    aria-label="Models"
-                                >
-                                    <Bot className="size-5" />
-                                </Button>
-                            </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
-                                Models
-                            </TooltipContent>
-                        </Tooltip>
                         <Tooltip>
                             <TooltipTrigger asChild>
                                 <Button
@@ -255,8 +202,8 @@ export default function Dashboard() {
                     </nav>
                 </aside>
                 <div className="flex flex-col">
-                    <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
-                        <h1 className="text-xl font-semibold">Playground</h1>
+                    <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b-2 bg-background px-4">
+                        <h1 className="text-xl font-semibold">AI Customer Support</h1>
                         <Drawer>
                             <DrawerTrigger asChild>
                                 <Button variant="ghost" size="icon" className="md:hidden">
@@ -271,7 +218,7 @@ export default function Dashboard() {
                                         Configure the settings for the model and messages.
                                     </DrawerDescription>
                                 </DrawerHeader>
-                                <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+                                {/* <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
                                     <fieldset className="grid gap-6 rounded-lg border p-4">
                                         <legend className="-ml-1 px-1 text-sm font-medium">
                                             Settings
@@ -373,23 +320,22 @@ export default function Dashboard() {
                                             <Textarea id="content" placeholder="You are a..." />
                                         </div>
                                     </fieldset>
-                                </form>
+                                </form> */}
                             </DrawerContent>
                         </Drawer>
                         <Button
                             variant="outline"
                             size="sm"
-                            className="ml-auto gap-1.5 text-sm"
+                            className="ml-auto gap-1.5 text-sm border-2 shadow-none"
                         >
                             <Share className="size-3.5" />
                             Share
                         </Button>
                     </header>
-                    <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-                        <div
+                    {/* <div
                             className="relative hidden flex-col items-start gap-8 md:flex" x-chunk="dashboard-03-chunk-0"
-                        >
-                            <form className="grid w-full items-start gap-6">
+                        > */}
+                    {/* <form className="grid w-full items-start gap-6">
                                 <fieldset className="grid gap-6 rounded-lg border p-4">
                                     <legend className="-ml-1 px-1 text-sm font-medium">
                                         Settings
@@ -496,44 +442,59 @@ export default function Dashboard() {
                                         />
                                     </div>
                                 </fieldset>
-                            </form>
-                        </div>
-                        <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-                            <Badge variant="outline" className="absolute right-3 top-3">
+                            </form> */}
+                    <main className="flex items-center justify-center flex-1 overflow-auto p-4">
+                        <div className="relative flex h-full w-full max-w-4xl min-h-[50vh] flex-col rounded-xl bg-inherit p-4 border-2 border-sky-500 shadow-xl">
+                            <Badge variant="outline" className="absolute right-3 top-3 border-2 border-sky-500 bg-sky-900/15">
                                 Output
                             </Badge>
                             <div className="flex-1" />
+                            {/*
+                                                <Stack spacing={2} direction="column" flexGrow={1} overflow="auto">
+                            //                     {
+                            //                         messages.map((message, index) => (
+                            //                             <Box
+                            //                                 key={index}
+                            //                                 display="flex"
+                            //                                 justifyContent={message.role === 'assistant' ? 'flex-start' : 'flex-end'}
+                            //                             >
+                            //                                 <Box
+                            //                                     bgcolor={message.role === 'assistant' ? 'primary.main' : 'secondary.main'}
+                            //                                     color="white"
+                            //                                     borderRadius={16}
+                            //                                     padding={5}
+                            //                                 >
+                            //                                     {message.content}
+                            //                                 </Box>
+                            //                             </Box>
+                            //                         ))
+                            //                     }
+                            //                 </Stack>
+                            */}
                             <form
-                                className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring" x-chunk="dashboard-03-chunk-1"
+                                className="relative overflow-hidden rounded-lg border-2 border-white bg-inherit focus-within:border-2 focus-within:border-sky-500" x-chunk="dashboard-03-chunk-1"
                             >
-                                <Label htmlFor="message" className="sr-only">
+                                <Label htmlFor="message" className="sr-only text-white">
                                     Message
                                 </Label>
                                 <Textarea
                                     id="message"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
                                     placeholder="Type your message here..."
-                                    className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+                                    className="min-h-12 resize-none border-0 bg-inherit p-3 focus-visible:ring-0"
                                 />
                                 <div className="flex items-center p-3 pt-0">
                                     <Tooltip>
                                         <TooltipTrigger asChild>
-                                            <Button variant="ghost" size="icon">
+                                            <Button className="text-white bg-sky-900/15 border-2 rounded-full border-sky-500" variant="ghost" size="icon">
                                                 <Paperclip className="size-4" />
                                                 <span className="sr-only">Attach file</span>
                                             </Button>
                                         </TooltipTrigger>
-                                        <TooltipContent side="top">Attach File</TooltipContent>
+                                        <TooltipContent side="right">Attach File</TooltipContent>
                                     </Tooltip>
-                                    <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <Button variant="ghost" size="icon">
-                                                <Mic className="size-4" />
-                                                <span className="sr-only">Use Microphone</span>
-                                            </Button>
-                                        </TooltipTrigger>
-                                        <TooltipContent side="top">Use Microphone</TooltipContent>
-                                    </Tooltip>
-                                    <Button type="submit" size="sm" className="ml-auto gap-1.5">
+                                    <Button type="submit" onClick={sendMessage} size="sm" variant="ghost" className="ml-auto gap-1.5 text-white font-bold shadow-none border-2 rounded-full bg-sky-900/15 border-sky-500 hover:shadow-none">
                                         Send Message
                                         <CornerDownLeft className="size-3.5" />
                                     </Button>
